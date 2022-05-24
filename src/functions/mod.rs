8a3ad53c589c495d99191eb554cee5e0e3b40701
Vritul Chauhan <vritul.chauhan@314ecorp.com>
2022-05-24 17:32:57 +0530

    potential fixes

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,4 +1,7 @@
-pub mod csv_function;
-pub mod path_variables;
+mod csv_function;
+// mod path_variables;  <-- This file isnt needed
+// only transaction update and structure are being used in outside
+// module(main), so others do not need to be declared as public.
+// They're in the same module as have full accessibility
 pub mod structures;
 pub mod transaction_update;
