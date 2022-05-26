3bfe779be50776b571c9714cf0cf616f8ba6433f
aarshiv-singh <harsh.kumar@314ecorp.com>
2022-05-26 11:42:46 +0530

    updated comments

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,8 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    // no need to use f64 as it only increases the accuracy after the decimal
-    // its faster to just use f32 when you know you dont need that much accurate decimal value
     let amount_in_float: f32 = args.amount.parse().expect("Invalid Amount");
     let transaction_type = args.transaction_type.parse::<TransactionType>().unwrap();
-    //you dont need to manually panic the program.
     transaction_update(&args.user, transaction_type, amount_in_float);
-    // instead of having a top level error message, you can put error message on the lower levels.
     println!("Transaction successful");
 }
